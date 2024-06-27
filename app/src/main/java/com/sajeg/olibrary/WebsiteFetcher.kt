be32d0ef747c472c06b28580b9714e97e4ca88a7
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-06-27 09:21:38 +0200

    Fixed an issue with the search results

--- app/src/main/java/com/sajeg/olibrary/WebsiteFetcher.kt
+++ app/src/main/java/com/sajeg/olibrary/WebsiteFetcher.kt
@@ -16,9 +16,11 @@ object WebsiteFetcher {
         return withContext(Dispatchers.IO) {
             try {
                 val output = mutableListOf<Book>()
-                val websiteUrl = URL("https://www.stadtbibliothek.oldenburg.de" +
-                        "/olsuchergebnisse?p_r_p_arena_urn%3Aarena_" +
-                        "search_query=${query.replace(" ", "+")}")
+                val websiteUrl = URL(
+                    "https://www.stadtbibliothek.oldenburg.de" +
+                            "/olsuchergebnisse?p_r_p_arena_urn%3Aarena_" +
+                            "search_query=${query.replace(" ", "+")}"
+                )
                 val connection = websiteUrl.openConnection() as HttpURLConnection
                 connection.instanceFollowRedirects = true
 
@@ -26,23 +28,41 @@ object WebsiteFetcher {
 
 
                 lastDocument = Jsoup.parse(inputStream.bufferedReader().use { it.readText() })
-                val title: List<Element> = lastDocument.select("div.arena-record-title a span").toList()
-                val author: List<Element> = lastDocument.select("div.arena-record-author span.arena-value").toList()
-                val year: List<Element> = lastDocument.select("div.arena-record-year span.arena-value").toList()
-                val language: List<Element> = lastDocument.select("div.arena-record-language span.arena-value span span").toList()
-                val genre: List<Element> = lastDocument.select("div.arena-record-genre span.arena-value").toList()
-                val image: List<String> = lastDocument.select("div.arena-book-jacket a img").map { it.attr("src") }.toList()
+                val title: List<Element> =
+                    lastDocument.select("div.arena-record-title a span").toList()
+                val author: List<Element> =
+                    lastDocument.select("div.arena-record-author span.arena-value").toList()
+                val year: List<Element> =
+                    lastDocument.select("div.arena-record-year span.arena-value").toList()
+                val language: List<Element> =
+                    lastDocument.select("div.arena-record-language span.arena-value span span")
+                        .toList()
+                val genre: List<Element> =
+                    lastDocument.select("div.arena-record-genre span.arena-value").toList()
+                val image: List<String> =
+                    lastDocument.select("div.arena-book-jacket a img").map { it.attr("src") }
+                        .toList()
 
-                for (i in title.indices) {
+                for (i in 0..<
+                        minOf(
+                            title.size,
+                            author.size,
+                            year.size,
+                            language.size,
+                            genre.size,
+                            image.size
+                        )) {
                     Log.d("Image", image[i])
-                    output.add(Book(
-                        title = title[i].text(),
-                        author = author[i].text(),
-                        year = year[i].text(),
-                        language = language[i].text(),
-                        genre = genre[i].text(),
-                        imageLink = "https://www.stadtbibliothek.oldenburg.de" + image[i]
-                    ))
+                    output.add(
+                        Book(
+                            title = title[i].text(),
+                            author = author[i].text(),
+                            year = year[i].text(),
+                            language = language[i].text(),
+                            genre = genre[i].text(),
+                            imageLink = "https://www.stadtbibliothek.oldenburg.de" + image[i]
+                        )
+                    )
                 }
 
                 return@withContext output
